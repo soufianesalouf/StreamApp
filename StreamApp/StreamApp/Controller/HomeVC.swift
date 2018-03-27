35bdf1294a43dad8a59dd6bf32b6e2c371ab3b82
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-27 19:05:28 +0100

    Building User Interface
    HomeVC

---
+++ StreamApp/StreamApp/Controller/HomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  StreamApp
+//
+//  Created by Soufiane Salouf on 3/27/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+}
+
---
+++ StreamApp/StreamApp/View/TrackCell.swift
@@ -0,0 +1,17 @@
+//
+//  TrackCell.swift
+//  StreamApp
+//
+//  Created by Soufiane Salouf on 3/27/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+    
+    //Outlets
+    @IBOutlet weak var trackTitle: UILabel!
+    
+
+}
